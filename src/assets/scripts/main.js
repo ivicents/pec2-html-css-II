3fb8deca2726a9954f20babe5c5a8cb755851dbf
ignacio <ignasi44@hotmail.com>
2021-04-20 17:56:16 +0200

    terminado blog y gallery

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -16,3 +16,17 @@ import * as bootstrap from 'bootstrap';
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 })();
+
+//Buscamos el boton que activara el toast
+var button = document.getElementById("toastBtn");
+//Le añadimos el evento para que al pinchar sobre el, lance la funcion showToast
+button.addEventListener("click", showToast)
+//Funcion que se ejecuta al pinchar sobre el boton de mostrar toast
+function showToast() {
+  //Buscamos el toast (contenido)
+  var myAlert = document.getElementById('toastNotice');
+  //Inicializamos el toast
+  var bsAlert = new bootstrap.Toast(myAlert);
+  //Mostramos el toast
+  bsAlert.show();
+}
